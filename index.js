7ff7a45ee27c2e1187427d712f99a585aeb7a003
chilbert <chase@chasehilbert.com>
2019-09-05 16:46:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, b) {
+  let cards = []
+  for (var i = 0; i < array.length; i++) {
+    cards.push(`Thank you, ${array[i]}, for the wonderful ${b} gift!`);
+  }
+  return cards;
+
+}
+
+
+function countdown(a) {
+  while (a >= 0) {
+    console.log(a--);
+  }
+}
